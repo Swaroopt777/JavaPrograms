3088e9b73ed171fff4b59614df52261ba8ca7d0d
unknown <Swaroopt4@gmail.com>
2024-10-24 10:54:48 +0530

    DemoString

---
+++ DemoString.java
@@ -0,0 +1,15 @@
+class  DemoString
+{
+	public static void main(String[] args) 
+	{
+		String s1="siddu";
+		String s2="siddu";
+		
+		if(s1.equals(s2)){
+		System.out.println("Strings are equal");
+		}
+		else{
+		System.out.println("Strings are unequal");	
+		}
+	}
+}
